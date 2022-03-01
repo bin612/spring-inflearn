00113675e016be6c6c36e8c990d58f1ec6b64f07
kimsangbin <tkdqls612@naver.com>
2022-03-01 17:05:36 +0900

    model Map으로 구현

---
+++ src/main/java/com/example/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,29 @@
+package com.example.servlet.web.frontcontroller.v4.controller;
+
+import com.example.servlet.domain.member.Member;
+import com.example.servlet.domain.member.MemberRepository;
+import com.example.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberSaveControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+
+        // memberRepository에 회원 저장
+        memberRepository.save(member);
+
+        // 저장한 회원의 정보를 member에 저장
+        model.put("member", member);
+
+        return "save-result";
+
+    }
+}
